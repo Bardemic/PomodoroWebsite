34e7dc4441185bea4bcd1cc027d9c4f5e89c4a0a
Bardemic <134565367+Bardemic@users.noreply.github.com>
2023-07-13 21:50:37 -0500

    Add Progress Bar

--- script.js
+++ script.js
@@ -1,9 +1,14 @@
+const progressBar = document.querySelector("#timeLeftProgress");
+
+
 var timerLength = 25;
 
 function startTimer(secondsLength){
+    progressBar.max = secondsLength;
     var timer = setInterval(function(){
         let minutes = parseInt(secondsLength / 60, 10);
         let seconds = parseInt(secondsLength % 60, 10);
+        progressBar.value++;
         console.log(minutes + " " + seconds)
 
         if(--secondsLength < 0) {
@@ -15,4 +20,4 @@ function startTimer(secondsLength){
 
 
 
-startTimer(3);
+startTimer(timerLength);
