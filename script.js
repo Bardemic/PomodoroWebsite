fedf8103300c5cb79a4e0e66d8e555d2e6e125f0
Bardemic <134565367+Bardemic@users.noreply.github.com>
2023-07-14 18:06:24 -0500

    Change Progress Bar to SVG Circle

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
-const progressBar = document.querySelector('#timeLeftProgress');
 const timer = document.querySelector('#timer');
 const start = document.querySelector('#startPause');
+const svgCircle = document.querySelector('circle');
 
 
 var studyTimeLength = 25;
@@ -8,7 +8,8 @@ var restTimeLength = 5;
 
 
 function startTimer(secondsLength, timerDiv){
-    progressBar.max = secondsLength;
+    let circleOffset = 450.0;
+    let circleInterval = circleOffset/secondsLength;
     var timer = setInterval(function(){
         let minutes = parseInt(secondsLength / 60, 10);
         let seconds = parseInt(secondsLength % 60, 10);
@@ -16,7 +17,8 @@ function startTimer(secondsLength, timerDiv){
         minutes = minutes < 10 ? '0' + minutes : minutes;
         seconds = seconds < 10 ? '0' + seconds : seconds;
 
-        progressBar.value++;
+        circleOffset -= circleInterval;
+        svgCircle.style.strokeDashoffset = circleOffset;
         timerDiv.innerHTML = `${minutes}:${seconds}`;
         console.log(minutes + ' ' + seconds)
 
